123c5f7374c9a2ad7869f5b111480a51bd57c363
yves93ets <ygabriel@kuvasystems.com>
2024-01-23 20:44:40 -0500

    added read me

--- app/item/create/page.tsx
+++ app/item/create/page.tsx
@@ -1,9 +1,22 @@
+"use client";
+import { useRouter } from "next/navigation";
+import { ArrowLeftCircle } from "lucide-react";
 import CreateItemForm from "@/app/components/CreateItemForm";
+import { Button } from "react-bootstrap";
 
-export default async function Item() {
+export default function Item() {
+  const router = useRouter();
   return (
-    <main>
-      <CreateItemForm />
+    <main className="main">
+      <div className="center mt-10">
+        <Button className="button" onClick={() => router.push("/")}>
+          <div className="button gap-2">
+            <ArrowLeftCircle />
+            dashboard
+          </div>
+        </Button>
+        <CreateItemForm />
+      </div>
     </main>
   );
 }
